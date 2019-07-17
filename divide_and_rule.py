86586b23b41e518a46173255122a68cf97485327
Sherkhan <sherkhancrs@gmail.com>
2019-07-17 21:40:33 +0600

    Finding length of array by recursion

--- divide_and_rule.py
+++ divide_and_rule.py
@@ -6,3 +6,11 @@ def summa(arr):
 
 
 print(summa([5, 6, 4, 15]))
+
+
+def count(arr):
+    if arr == []:
+        return 0
+    else:
+        return 1 + count(arr[1:])
+print(count([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
